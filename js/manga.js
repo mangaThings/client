50277d7414c8e8e8b9b38fedf47b7427decf84cb
straf16 <depoksugutamu621@gmail.com>
2019-10-04 15:05:44 +0700

    add chart

--- js/manga.js
+++ js/manga.js
@@ -66,6 +66,26 @@ function getDetail(id) {
         <div class="summary">${synopsis}</div>
       </div>
     </div>
+    <button class="btn btn-warning mt-3" onclick="getChart(${id})" data-toggle="modal" data-target="#chart">Show Chart</button>
+
+    <div id="chart" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Comments Chart</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+          <div class="image-chart">
+          
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
 
   `;
 
